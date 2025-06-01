51c48b083694cc4a0a618ece3f5077b41e4a88ab
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-06-01 15:42:22 +0400

    Non-clustered on Location → Filter by location (Library Table)

--- Indexes.sql
+++ Indexes.sql
@@ -5,3 +5,6 @@ USE LibraryDatabase;
 -- Library Table
 -- • Non-clustered on Name → Search by name
 CREATE NONCLUSTERED INDEX Library_Name ON Library(L_Name);
+
+-- • Non-clustered on Location → Filter by location
+CREATE NONCLUSTERED INDEX Library_Location ON Library(Location);
