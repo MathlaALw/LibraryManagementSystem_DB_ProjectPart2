699cb38cd37614b561445405329c80b2ad37575c
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-06-01 15:44:47 +0400

    Clustered on LibraryID, ISBN → Lookup by book in specific library (Book Table)

--- Indexes.sql
+++ Indexes.sql
@@ -8,3 +8,14 @@ CREATE NONCLUSTERED INDEX Library_Name ON Library(L_Name);
 
 -- • Non-clustered on Location → Filter by location
 CREATE NONCLUSTERED INDEX Library_Location ON Library(Location);
+
+
+
+
+-- Book Table
+-- • Clustered on LibraryID, ISBN → Lookup by book in specific library
+
+CREATE NONCLUSTERED INDEX Book_ISBN ON Book(ISBN);
+CREATE CLUSTERED INDEX LibraryBook_LibraryID_BookID ON LibraryBook(L_ID, Book_ID);
+
+
