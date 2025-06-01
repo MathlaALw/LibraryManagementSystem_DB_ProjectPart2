971ae197082967f20b05340c9bd54fb2f77d6659
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-06-01 15:41:36 +0400

    Non-clustered on Name → Search by name (Library Table)

--- Indexes.sql
+++ Indexes.sql
@@ -1,1 +1,7 @@
-Indexes.sql
+﻿-- 1. Indexing Strategy – Performance Optimization
+-- Apply indexes to speed up commonly-used queries:
+
+USE LibraryDatabase;
+-- Library Table
+-- • Non-clustered on Name → Search by name
+CREATE NONCLUSTERED INDEX Library_Name ON Library(L_Name);
