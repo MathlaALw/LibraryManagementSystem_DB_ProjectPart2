40730f43b884ec2c1c02d54f61aa61c010b29d6c
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-06-01 16:59:23 +0400

    ViewLoanStatusSummary Loan stats (issued, returned, overdue) per library

--- Views.sql
+++ Views.sql
@@ -4,4 +4,37 @@ FROM Book B
 LEFT JOIN ReviewBook RB ON B.Book_ID = RB.Book_ID
 LEFT JOIN Loan L ON B.Book_ID = L.Book_ID
 GROUP BY B.Book_ID, B.Title, B.ISBN
-HAVING AVG(RB.Rating) > 4.5;SELECT * FROM ViewPopularBooks;-- ViewMemberLoanSummary  ->  Member loan count + total fines paid
+HAVING AVG(RB.Rating) > 4.5;SELECT * FROM ViewPopularBooks;-- ViewMemberLoanSummary  ->  Member loan count + total fines paidCREATE VIEW ViewMemberLoanSummary AS
+SELECT M.M_ID,M.FName + ' ' + M.LName AS 'MEMBER NAME ',COUNT(l.Loan_Date) AS 'TOTAL LOANS',ISNULL(SUM(p.Amount), 0) AS 'TOTAL FINES'
+FROM Members M
+LEFT JOIN Loan L ON M.M_ID = L.M_ID
+LEFT JOIN Payment P ON M.M_ID = P.M_ID AND P.Loan_Date = L.Loan_Date AND P.Book_ID = L.Book_ID
+GROUP BY M.M_ID, M.FName, M.LName;
+
+SELECT * FROM ViewMemberLoanSummary;
+
+-- ViewAvailableBooks  ->  Available books grouped by genre, ordered by price
+
+
+CREATE VIEW ViewAvailableBooks AS
+SELECT Genre, Title, Shelf_Location,Price
+FROM Book
+WHERE Available_State = 'TRUE'
+
+
+SELECT * FROM ViewAvailableBooks
+ORDER BY Price , Genre;
+  
+
+-- ViewLoanStatusSummary Loan stats (issued, returned, overdue) per library 
+
+CREATE VIEW ViewLoanStatusSummary AS
+SELECT LB.L_ID, L.Status, COUNT(*) AS 'STATUS COUNT'
+FROM Loan L
+LEFT JOIN Book B ON L.Book_ID = B.Book_ID
+LEFT JOIN LibraryBook LB ON B.Book_ID = LB.Book_ID
+GROUP BY LB.L_ID, L.Status;
+
+SELECT * FROM ViewLoanStatusSummary;
+
+
