b3eee46080dcce4da39816d44be835ced070809c
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-06-02 07:16:55 +0400

    ViewLoanStatusSummary Loan stats (issued, returned, overdue) per library

--- Views.sql
+++ Views.sql
@@ -1,10 +1,20 @@
--- 2. Views – Frontend Integration Support-- ViewPopularBooks  -> Books with average rating > 4.5 + total loansCREATE VIEW ViewPopularBooks AS
+-- 2. Views – Frontend Integration Support
+
+-- ViewPopularBooks  -> Books with average rating > 4.5 + total loans
+
+CREATE VIEW ViewPopularBooks AS
 SELECT B.Book_ID,B.Title,b.ISBN, AVG(RB.Rating) AS 'AVERAGE RATING', COUNT(L.Loan_Date) AS TotalLoans
 FROM Book B
 LEFT JOIN ReviewBook RB ON B.Book_ID = RB.Book_ID
 LEFT JOIN Loan L ON B.Book_ID = L.Book_ID
 GROUP BY B.Book_ID, B.Title, B.ISBN
-HAVING AVG(RB.Rating) > 4.5;SELECT * FROM ViewPopularBooks;-- ViewMemberLoanSummary  ->  Member loan count + total fines paidCREATE VIEW ViewMemberLoanSummary AS
+HAVING AVG(RB.Rating) > 4.5;
+
+SELECT * FROM ViewPopularBooks;
+
+-- ViewMemberLoanSummary  ->  Member loan count + total fines paid
+
+CREATE VIEW ViewMemberLoanSummary AS
 SELECT M.M_ID,M.FName + ' ' + M.LName AS 'MEMBER NAME ',COUNT(l.Loan_Date) AS 'TOTAL LOANS',ISNULL(SUM(p.Amount), 0) AS 'TOTAL FINES'
 FROM Members M
 LEFT JOIN Loan L ON M.M_ID = L.M_ID
